ffca99ac58cf47efb16b987da09cfa001a3fbc83
Anas <anassaif.507@gmail.com>
2024-03-12 22:16:38 +0530

    m

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// app.use(bodyParser.urlencoded({ extended: true })); // Middleware to parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); // Middleware to parse application/x-www-form-urlencoded
 app.use(express.json());
 
 app.get('/', (req, res) => {
