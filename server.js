c0b70203c471aaf2527b7440d84e73a08da8ba55
Anas <anassaif.507@gmail.com>
2024-03-24 17:19:06 +0530

    updated

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Server } = require("socket.io");
 const SensorReading = require('./models/sensorReadingModel');
-const jwt = require("jsonwebtoken");
-const passport = require('passport');
-const http = require('http'); // Import the HTTP module
-// const { saveSensorReading } = require('./controllers/sensorController');
+const http = require('http');
 const dbConnect = require("./config/database");
-require("./config/passport")
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -16,8 +12,7 @@ const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(passport.initialize());
-// app.use(passport.authenticate("google", {session:false}));
+
 
 const server = http.createServer(app); // Create HTTP server
 const io = new Server(server, {
@@ -31,7 +26,7 @@ const PORT = process.env.PORT || 4000;
 
 
 const isProduction = process.env.NODE_ENV === "production";
-const CLIENT_URL = isProduction ? "https://weather-app-frontend-kohl.vercel.app":"http://localhost:3000";
+// const CLIENT_URL = isProduction ? "https://weather-app-frontend-kohl.vercel.app":"http://localhost:3000";
 
 
 app.get('/', (req, res) => {
@@ -39,50 +34,14 @@ app.get('/', (req, res) => {
   res.status(200).send('Server called successfully');
 });
 
-app.get('/success', (req, res) => {
-  console.log('Server called successfully');
-  res.status(200).send('Server called successfully');
-});
-
-
-app.get('/failure', (req, res) => {
-  console.log('LolServer called successfully');
-  res.status(401).send('Failed..');
-});
-
-
-
-
-// Use the controller function to handle POST requests for sensor readings
 app.post('/', saveSensorReading);
-
-// Google OAuth authentication routes
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"], session:false }));
-
-app.get('/auth/googleRedirect', 
-  passport.authenticate('google', { failureRedirect: '/failure', session:false  }),
-  function(req, res) {
-       const token = jwt.sign({ userId: "Anas Saif", email: "anassaif@.com" }, process.env.JWT_SECRET, { expiresIn: "8h" });
-       console.log("Userrrr",req.user)
-        // Set the token as a cookie
-        // res.cookie('jwtToken', token, { httpOnly: true });
-        // Redirect back to the React application
-        // res.redirect(`${CLIENT_URL}/secret?token=${token}`);
-        res.redirect(`${CLIENT_URL}`);
-  });
-
-
 dbConnect();
+
 
 io.on("connection", async (socket) => {
   console.log(`User connected: ${socket.id}`);
-
-
-  // console.log(socket);
-  // Emit a test message to the client upon connection
   socket.emit('testMessage', { message: 'Hello from the server!' });
   console.log("Test message emitted to client.");
-
   socket.on("lol", async (data, callback) => {
     console.log("Lol....", data);
     const dataaa = {message:'Hello CALLBACK...'}
