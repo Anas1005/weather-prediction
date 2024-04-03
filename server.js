3283b5f3f49063d48d3baf57daeaaec9560fc365
Anas <anassaif.507@gmail.com>
2024-04-03 21:37:41 +0530

    updated

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const http = require('http');
 const axios = require("axios");
 const dbConnect = require("./config/database");
 const dotenv = require("dotenv");
+const cors=require("cors")
 dotenv.config();
 
 const app = express();
